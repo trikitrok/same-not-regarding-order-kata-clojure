e7e7db6ff6b2ac09d018c22286e7a336c6d0b02c
trikitrok <trikitrok@gmail.com>
2018-03-25 14:44:44 +0200

    separating properties that work with and without duplicates

--- test/same_not_regarding_order/core_test.clj
+++ test/same_not_regarding_order/core_test.clj
@@ -8,11 +8,21 @@
 (facts
   "about identifying when sequence elements are the same not regarding order"
 
-  (for-all
-    [elems (gen/vector gen/int)]
-    {:num-tests 1000}
-    (fact "when they are"
-      (sut/same-not-regarding-order? elems (shuffle elems)) => true))
+  (facts "when they are"
+    (for-all
+      [elems (gen/vector-distinct gen/int)]
+      {:num-tests 1000}
+      (fact "without duplicates"
+        (sut/same-not-regarding-order? elems (shuffle elems)) => true))
+
+    (for-all
+      [elems (gen/let [v (gen/not-empty (gen/vector gen/int))
+                       t (gen/tuple (gen/such-that #(> % 1) gen/s-pos-int) gen/int)]
+                      (->> (apply repeat t)
+                           (concat v)
+                           shuffle))]
+      (fact "with duplicates"
+        (sut/same-not-regarding-order? elems (shuffle elems)) => true)))
 
   (for-all
     [[elems1 elems2] (gen/list-distinct (gen/vector gen/int) {:num-elements 2})]
