cca25e34da7cc76e8a66782f4da82875f4ffd2cc
trikitrok <trikitrok@gmail.com>
2018-03-25 14:09:28 +0200

    substituting an example test by a property test

--- test/same_not_regarding_order/core_test.clj
+++ test/same_not_regarding_order/core_test.clj
@@ -8,16 +8,19 @@
 (facts
   "about identifying when sequence elements are the same not regarding order"
 
-  (fact
-    "when they are not"
-    (sut/same-not-regarding-order?
-      [1 2 3] [14]) => false
-
-    (sut/same-not-regarding-order?
-      [1 2 3] [1 1 2 3]) => false)
-
   (for-all
     [elems (gen/vector gen/int)]
-    {:num-tests 100}
+    {:num-tests 1000}
     (fact "when they are"
-      (sut/same-not-regarding-order? elems (shuffle elems)) => true)))
+      (sut/same-not-regarding-order? elems (shuffle elems)) => true))
+
+  (for-all
+    [[elems1 elems2] (gen/list-distinct (gen/vector gen/int) {:num-elements 2})]
+    {:num-tests 1000}
+    (fact "when they are not"
+      (sut/same-not-regarding-order? elems1 elems2) => false))
+
+  (fact
+    "when they are not because of repeated elements"
+    (sut/same-not-regarding-order?
+      [1 2 3] [1 1 2 3]) => false))
